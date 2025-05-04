35cf17ac961ff9994300a946293eec7536f74478
Herick <herickwill@hotmail.com>
2025-05-04 18:39:25 -0300

    adicionando conexao do esp32 com a aws com medicoes

--- lambda/app/handler.py
+++ lambda/app/handler.py
@@ -1,8 +1,20 @@
 # Entry point da Lambda
 import json
+import boto3
+import os
 
 def lambda_handler(event, context):
+    dynamodb = boto3.client('dynamodb')
+    print("Medição recebida!")
+    dynamodb.put_item(
+    TableName=os.environ['DYNAMODB_TABLE'],
+    Item={
+        'caixa_id': {'S': str(event.get("caixa_id"))},
+        'timestamp': {'S': str(event.get("timestamp"))},
+        'valor_sensor': {'N': str(event.get("valor_sensor"))}
+        }
+    )
     return {
         'statusCode': 200,
-        'body': json.dumps('Testando Gustavo Emilly')
-}
+        'body': json.dumps(event['caixa_id'])
+    }
